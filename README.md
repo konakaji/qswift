d9ca23fdb8ffdf5a0bda8f19d6cc53f5593b31cd
knakaji <nakajijiji@gmail.com>
2023-05-13 19:40:22 -0400

    add setup.py

---
+++ README.md
@@ -0,0 +1,1 @@
+# qswift-encoder
--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="qwrapper",
-    version="0.4.9",
+    name="qswiftencoder",
+    version="0.0.1",
     author="kouhei nakaji",
     author_email="nakajijiji@gmail.com",
     description="You can receive the message 'Hello!!!'",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/konakaji/qwrapper",
+    url="https://github.com/konakaji/qswiftencoder",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -19,11 +19,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     install_requires=[
-        "qiskit>=0.30.0",
-        "Qulacs>=0.3.0",
-        "matplotlib>=3.0.0",
-        "pylatexenc>=2.0",
-        "qutip>=4.7.0"
+        "qwrapper @ git+ssh://git@github.com/konakaji/qwrapper.git@ab189953740330f759ab4ba3e780ce7b087ab902"
     ],
     python_requires='>=3.6',
 )
