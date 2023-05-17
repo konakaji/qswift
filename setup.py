5e575e371b2f93ddd715ae5073f85028e2da8ffa
knakaji <nakajijiji@gmail.com>
2023-05-17 16:20:00 -0400

    increment version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="qswift",
-    version="0.0.6",
+    version="0.0.7",
     author="kouhei nakaji",
     author_email="nakajijiji@gmail.com",
     description="You can receive the message 'Hello!!!'",
